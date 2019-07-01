85c63a4727500c79b024f51a527968c2ab861297
Malik Kamran Abid <30325523+malikkamranabid@users.noreply.github.com>
2019-07-01 22:36:14 +0500

    added package functionality for mysql (#1)
    * added package functionality
    
    * [53] removed necessary thing from git ignore file
    
    * [53] replace all the R5.out.err to R5.out.error
    
    * [53] added test script
    
    * [53]  added param config
    
    * [53] run standard.js
    
    * [53] updated version
    
    * [53] updated read to read config and write to write config

---
+++ index.js
@@ -0,0 +1,107 @@
+/* eslint-disable brace-style, camelcase, semi */
+/* global R5 */
+
+module.exports = Database;
+
+if (!global.R5) {
+  global.R5 = {
+    out: console
+  }
+}
+
+let mysql = require('mysql');
+
+// Constructor
+
+function Database (read_config, write_config) {
+  const config = {};
+  config.read = read_config;
+  config.write = write_config;
+  this.IN = new Host(config['read']);
+
+  if (config['read']['host'] !== config['write']['host']) {
+    this.OUT = new Host(config);
+  }
+  else {
+    this.OUT = this.IN;
+  }
+}
+
+function Host (config) {
+  this.error_retries = 0;
+  this.error_timeout = 10000;
+  this.config = config;
+  connect(this);
+}
+
+// Public Methods
+
+Database.prototype.query = function (query, callback) {
+  let host = this.IN;
+  if (
+    (typeof query === 'string' && is_update(query)) ||
+    (typeof query === 'object' && is_update(query.sql))
+  ) {
+    host = this.OUT;
+  }
+  host.query(query, callback);
+};
+
+Host.prototype = {
+  query: function (query, callback) {
+    let host = this;
+    host.connection.query(query, function (err, results, fields) {
+      if (err && err.fatal) {
+        host.retry(query, callback);
+      }
+      else if (typeof callback === 'function') {
+        if (err) { R5.out.error(`Query error: ${err}\n${query}\n`); }
+        callback(err, results, fields);
+      }
+    });
+  },
+
+  retry: function (query, callback) {
+    let host = this;
+    setTimeout(function () {
+      host.connection.destroy();
+      connect(host);
+      if (query) { host.query(query, callback); }
+    }, host.error_timeout * (host.error_retries + 1));
+  }
+};
+
+// Private Methods
+
+function connect (host) {
+  host.connection = mysql.createConnection(host.config);
+
+  host.connection.connect(function (err) {
+    if (err) {
+      if (host.error_retries++ < 10) {
+        R5.out.error(`connecting (retrying [${host.error_retries}]): ${err.code}`);
+        host.retry();
+        return;
+      }
+      R5.out.error(`connecting: ${err.stack}`);
+      throw err;
+    }
+    R5.out.log(`Connected to MySQL (conn: ${host.connection.threadId})`);
+    host.error_retries = 0;
+  });
+
+  host.connection.on('error', function (err) {
+    R5.out.error((err.fatal === true ? '(FATAL) ' : '') + err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.fatal === true) {
+      host.connection.destroy();
+      connect(host);
+    }
+    else {
+      throw err;
+    }
+  });
+}
+
+function is_update (str) {
+  return str.substring(0, 6).toUpperCase() !== 'SELECT';
+}
---
+++ test.js
@@ -0,0 +1,35 @@
+/* eslint-disable brace-style, camelcase, semi */
+/* eslint-env mocha */
+
+require('dotenv').config();
+
+var chai = require('chai');
+var assert = chai.assert;
+const config = {
+  read: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'funnode',
+    debug: false
+  },
+  write: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'funnode',
+    debug: false
+  }
+};
+describe('Database', function () {
+  let database = new (require('./index.js'))(config.read, config.write);
+
+  describe('Checking Database Connection', function () {
+    it('should not return errors', function (done) {
+      database.query('select version()', function (err, results, fields) {
+        assert.notEqual(typeof results, 'undefined', err);
+        done();
+      });
+    });
+  });
+});
